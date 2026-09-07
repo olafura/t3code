feca4b8c724c21130f15b982f8bf099ccf510a28
olafura <olafurara@gmail.com>
2026-09-07 16:08:25 +0200

    refactor(desktop-qt): hand theme override ownership to the web page

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -184,6 +184,7 @@ function RootRouteView() {
           <ContrastAppearanceSync />
           <GlassAppearanceSync />
           <FontAppearanceSync />
+          <ShellThemeBridge publishToShell={false} />
           <ConfirmDialogHost />
           {primaryEnvironmentAuthenticated ? <EventRouter skipInitialBootstrapNavigation /> : null}
           <Outlet />
