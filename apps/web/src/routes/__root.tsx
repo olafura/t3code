64e801bca3d6675004c49265a3606205c2a04449
olafura <olafurara@gmail.com>
2026-09-07 14:58:09 +0200

    fix(desktop-qt): reconcile web integration after upstream rebase

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -206,7 +206,7 @@ function RootRouteView() {
           <GlassAppearanceSync />
           <FontAppearanceSync />
           <ConfirmDialogHost />
-          {primaryEnvironmentAuthenticated ? <EventRouter /> : null}
+          {primaryEnvironmentAuthenticated ? <EventRouter skipInitialBootstrapNavigation /> : null}
           <Outlet />
         </AnchoredToastProvider>
       </ToastProvider>
